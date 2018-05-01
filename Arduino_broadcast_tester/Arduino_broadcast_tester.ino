409de80e9c28f0cfecf3fc735684a87d94f4b314
McNibbler <McNibbler@users.noreply.github.com>
2018-05-01 08:52:19 -0400

    added code from DAC controller and moved it here because it deserved its own repository

---
+++ Arduino_broadcast_tester/Arduino_broadcast_tester.ino
@@ -0,0 +1,24 @@
+/* Serial Communication Through USB Test
+ * Thomas Kaunzinger
+ * May 1, 2018
+ * 
+ * Garbage code that I can use to see if I can read from the arduino.
+ */
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  for (int i=0; i <= 10; i++){
+    Serial.println(i);
+    delay(10);
+  }
+
+  delay(200);
+  Serial.println("nice");
+  delay(3000);
+
+}
---
+++ Serial_Communication.py
@@ -0,0 +1,46 @@
+#####################################
+# Arduino Serial Communication Test #
+# Version: alpha 0.1                #
+# Thomas Kaunzinger                 #
+# April 30, 2018                    #
+#                                   #
+# A dumb program used to test       #
+# serial communication between the  #
+# computer and Arduino for use      #
+# in future applications.           #
+#####################################
+
+import serial
+import time
+
+com_port = 'COM4'
+
+serial_port = serial.Serial(port=com_port, baudrate=9600)
+
+
+# Execution
+def main():
+    while True:
+        # send_voltage(0, 1.0)
+        print("Data:", readback_data())
+
+
+# I have to keep looking at the documentation. Supposed to send the DAC address and desired voltage through the COM port
+#   and tell the arduino what voltage it wants to send to the DAC.
+def send_voltage(dac_address, voltage):
+    serial_port.write(dac_address)
+    serial_port.write(voltage)
+
+
+# Reads back the data being sent by the arduino through the serial port
+def readback_data():
+    # Serial port recieves the string encoded in binary, thus needing decode(). Arduino also sends newline characters,
+    #   which we do not need, so that's why I've trimmed the string
+    return str(serial_port.readline().decode()[:-2])
+
+
+# Execute main method
+if __name__ == "__main__":
+    main()
+    print()
+    input("Press <Enter> to close...")
